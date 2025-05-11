4097c74b0fb76fe107801cd67adb9bfb9c3b403e
diya-thabet <mohamaddhia@gmail.com>
2025-05-11 23:00:30 +0100

    dev

---
+++ backendKHATWA/src/main/java/fennec/khatwa/khatwa/KhatwaApplication.java
@@ -0,0 +1,13 @@
+package fennec.khatwa.khatwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhatwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhatwaApplication.class, args);
+	}
+
+}
---
+++ backendKHATWA/src/test/java/fennec/khatwa/khatwa/KhatwaApplicationTests.java
@@ -0,0 +1,13 @@
+package fennec.khatwa.khatwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhatwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
