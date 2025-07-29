56bd629b507813ade470f925037cb0e45482ca10
diya-thabet <mohamaddhia@gmail.com>
2025-07-29 12:44:26 +0100

    freelancer profile contreoller fix

--- Hirfa/src/main/java/fennec/khatwa/khatwa/controller/FreelancerProfileController.java
+++ Hirfa/src/main/java/fennec/khatwa/khatwa/controller/FreelancerProfileController.java
@@ -1,8 +1,10 @@
 package fennec.khatwa.khatwa.controller;
 
-
+import fennec.khatwa.khatwa.dto.FreelancerProfileDTO;
 import fennec.khatwa.khatwa.model.FreelancerProfile;
+import fennec.khatwa.khatwa.model.User;
 import fennec.khatwa.khatwa.service.FreelancerProfileService;
+import fennec.khatwa.khatwa.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,13 +17,24 @@ public class FreelancerProfileController {
     @Autowired
     private FreelancerProfileService freelancerProfileService;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/get-all-freelancers-profiles")
     public List<FreelancerProfile> getAllFreelancerProfiles() {
         return freelancerProfileService.getAllFreelancerProfiles();
     }
 
     @PostMapping("/create-freelancer-profile")
-    public FreelancerProfile createFreelancerProfile(@RequestBody FreelancerProfile profile) {
+    public FreelancerProfile createFreelancerProfile(@RequestBody FreelancerProfileDTO dto) {
+        User user = userRepository.findById(dto.getUserId())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        FreelancerProfile profile = new FreelancerProfile();
+        profile.setUser(user);
+        profile.setBio(dto.getBio());
+        profile.setSkills(dto.getSkills());
+
         return freelancerProfileService.createFreelancerProfile(profile);
     }
 }
